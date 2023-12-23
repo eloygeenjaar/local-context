8886b146dd065ac143474ed0d2549772eefc1e68
eloygeenjaar <geenjaareloy@gmail.com>
2023-12-23 15:22:31 +0000

    Some small fixes, the multi-array runs seemed to fail. Hopefully, these fixes allow them to finish running

--- configs/generate_configs.py
+++ configs/generate_configs.py
@@ -8,20 +8,22 @@ i = 0
 seeds = [42, 1337, 9999, 1212, 8585, 6767]
 models = ['DSVAE']
 local_sizes = [2, 4, 8]
-global_sizes = [2, 4, 8, 16]
+context_sizes = [2, 4, 8, 16]
 model_dict = default_conf.copy()
 for seed in seeds:
     for model in models:
         for local_size in local_sizes:
-            for global_size in global_sizes:
+            for context_size in context_sizes:
                     model_dict['seed'] = seed
                     model_dict['local_size'] = local_size
-                    model_dict['global_size'] = global_size
+                    model_dict['context_size'] = context_size
                     model_dict['model'] = model
                     with open(f'config_{i}.yaml', 'w') as file:
                         documents = yaml.dump(model_dict, file)
                     print(i,
-                            model_dict['global_size'])
+                          model_dict['seed'],
+                          model_dict['local_size'],
+                          model_dict['context_size'])
                     i += 1
 
 seeds = [42, 1337, 9999, 1212, 8585, 6767]
@@ -33,10 +35,10 @@ for seed in seeds:
         for local_size in local_sizes:
             model_dict['seed'] = seed
             model_dict['local_size'] = local_size
-            model_dict['global_size'] = 0
+            model_dict['context_size'] = 0
             model_dict['model'] = model
             with open(f'config_{i}.yaml', 'w') as file:
                 documents = yaml.dump(model_dict, file)
             print(i,
-                    model_dict['global_size'])
+                    model_dict['context_size'])
             i += 1
