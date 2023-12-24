a404814e31eb1beb5e783404f3e3529038051a92
eloygeenjaar <geenjaareloy@gmail.com>
2023-12-24 10:46:42 +0000

    Add config generation for CO model

--- configs/generate_configs.py
+++ configs/generate_configs.py
@@ -44,3 +44,22 @@ for seed in seeds:
                   model_dict['local_size'],
                   model_dict['context_size'])
             i += 1
+
+seeds = [42, 1337, 9999, 1212, 8585, 6767]
+models = ['CO']
+context_sizes = [2, 4, 8, 16]
+model_dict = default_conf.copy()
+for seed in seeds:
+    for model in models:
+        for context_size in context_sizes:
+            model_dict['seed'] = seed
+            model_dict['local_size'] = 0
+            model_dict['context_size'] = context_size
+            model_dict['model'] = model
+            with open(f'config_{i}.yaml', 'w') as file:
+                documents = yaml.dump(model_dict, file)
+            print(i,
+                  model_dict['seed'],
+                  model_dict['local_size'],
+                  model_dict['context_size'])
+            i += 1
