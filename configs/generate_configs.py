185dc16b592fa8ac2d5c92471042f1582d62363f
eloygeenjaar <geenjaareloy@gmail.com>
2024-02-08 17:45:57 +0000

    Experimenting with CFDSVAE and Upsampled

--- configs/generate_configs.py
+++ configs/generate_configs.py
@@ -5,28 +5,32 @@ with Path('./default.yaml').open('r') as f:
     default_conf = dict(yaml.safe_load(f))
 
 i = 0
-seeds = [42, 1337, 9999, 1212]
-models = ['DSVAE', 'CDSVAE', 'CIDSVAE', 'IDSVAE']
-local_sizes = [2, 4, 8]
-context_sizes = [2, 4, 8, 16]
+datasets = ['UpsampledICAfBIRN', 'ICAfBIRN']
+seeds = [42, 1337, 9999]
+models = ['DSVAE', 'CFDSVAE']
+local_sizes = [2]
+context_sizes = [2]
 model_dict = default_conf.copy()
-for seed in seeds:
-    for model in models:
-        for local_size in local_sizes:
-            for context_size in context_sizes:
-                model_dict['seed'] = seed
-                model_dict['local_size'] = local_size
-                model_dict['context_size'] = context_size
-                model_dict['model'] = model
-                with open(f'config_{i}.yaml', 'w') as file:
-                    documents = yaml.dump(model_dict, file)
-                print(i,
-                        model_dict['model'],
-                        model_dict['seed'],
-                        model_dict['local_size'],
-                        model_dict['context_size'])
-                i += 1
-
+for dataset in datasets:
+    for seed in seeds:
+        for model in models:
+            for local_size in local_sizes:
+                for context_size in context_sizes:
+                    model_dict['dataset'] = dataset
+                    model_dict['seed'] = seed
+                    model_dict['local_size'] = local_size
+                    model_dict['context_size'] = context_size
+                    model_dict['model'] = model
+                    with open(f'config_{i}.yaml', 'w') as file:
+                        documents = yaml.dump(model_dict, file)
+                    print(i,
+                            model_dict['dataset'],
+                            model_dict['model'],
+                            model_dict['seed'],
+                            model_dict['local_size'],
+                            model_dict['context_size'])
+                    i += 1
+quit()
 seeds = [42, 1337, 9999, 1212]
 models = ['LVAE']
 local_sizes = [2, 4, 8]
